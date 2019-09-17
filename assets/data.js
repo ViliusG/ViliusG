56fd386f391458d03ae303ccff625a66bd5e24b6
Vilius <vilius.grizas@techhuddle.com>
2019-09-17 20:55:58 +0300

    updated data and styles

--- assets/data.js
+++ assets/data.js
@@ -1,27 +1,37 @@
 var projects = [
 	{
 		name: 'Osprey Flight Solutions',
-		description: 'Most exciting project I have been a part of so far.<br>Building an API for company dealing with aviation security<br>Technologies used: PHP 7.1, Lumen 5.8, Docker, AWS',
+		description: 'The goal of the company is to ensure safe air travel by providing the neccesary information to airlines.<br>By scraping data from multiple sources and using probability algorithms and experienced people in the field they manage to get almost live data regarding flight safety world wide. Recently they have signed a contract with Delta airways - second largest airway company in the world.<br> My tasks include building API endpoints throughout the platform, scraping web for information, organising it, creating cron jobs, writing Unit tests, generating reports, managing user payment levels and more.',
+		summary: 'Most exciting project I have been a part of so far.<br>Building an API for company dealing with aviation security.',
+		technologies: 'PHP 7.2, Lumen 5.8, Docker, AWS, MySql, Swagger, Guzzle, Fractal, DomCrawler, TwitterAPI, Faker, PHPUnit, PHP Grump, BitBucket, Jira',
 		url: 'https://www.ospreyflightsolutions.com/'
 	},
 	{
 		name: 'Rio Aposta',
-		description: 'My first project in a company ever. Was mostly about learning the comunication. I did some minimal FE work.',
+		description: 'This company is using a general CMS made for betting type of websites and I had to make sume custom adjustments to the front end. This task was given as a first challenge to me to learn remote comunication with a client. I was the only developer from the company working with this product. Had to comunicate with both technical and non-technical people and got to introduce this project to later developers',
+		summary: 'My first project in a company ever. Internet betting and gambling platform made for Brasilian market',
+		technologies: 'HTML, CSS, CMS',
 		url: 'https://www.rioaposta.com/en/'
 	},
 	{
 		name: 'Intranet',
-		description: 'Built an intranet for an Italian software company. User and product management system. PHP 5.6, JavaScript, Leaflet.',
+		description: 'This was a freelance project. I had to build a software that the company was to sell. My tasks were building user role system (users had roles with rights and additional rights could be given to specific users) and tank management system. Tank could hold a specific material (sand, salt, explosives) and had a location. Few groups of tanks make up a plant that help with organising materials.',
+		summary: 'Built an intranet for an Italian software company.',
+		technologies: 'PHP 5.6, HTML, CSS, JavaScript, Ajax, Leaflet, Poedit, MySql',
 		url: ''
 	},
 	{
 		name: 'Vending Machine',
-		description: 'Test I got from my last employer. Core PHP, OOP, PHPUnit',
+		description: 'Build a simulation of a vending-machine. Accept coins, reject invalid coins, calculate change, give items.',
+		summary: 'Test sort of task I got from one of my employers',
+		technologies: 'PHP, PHPUnit',
 		url: 'https://github.com/ViliusG/vending-machine'
 	},
 	{
 		name: 'Binary Search Exercise',
-		description: 'An exercise I made up to practise some JavaScript and logic',
+		description: 'An exercise I made up to practise JavaScript. Simple bynary search to find a randomly selected number.',
+		summary: 'Binary search algorithm',
+		technologies: 'JavaScript',
 		url: 'https://github.com/ViliusG/binary-search'
 	}
 ]
--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,33 @@
 function printProjects(){
 	for (var i = 0; i < projects.length; i++) {
 		var string = '';
-		string += '<div class="project clearfix">';
+		string += '<div class="project clearfix divHover" onclick="openSummary(' + i + ')">';
 		string += '<h3>' + projects[i].name + '</h3>';
-		string += '<p>' + projects[i].description + '</p>'
-		if (projects[i].url != '') {
-			string += '<a class="right" href="' + projects[i].url + '" target="_blank" >see for yourself</a>';
-		}
+		string += '<p>' + projects[i].summary + '</p>'
 		string += '</div>';
 		document.getElementById('projects').innerHTML += string;
 	}
 }
 
+function openSummary(i){
+	document.getElementById('popUp').style.display="block";
+	var string = '<h2 class="center">' + projects[i].name + '</h2>';
+	string += '<p class="center">' + projects[i].description + '</p>';
+	string += '<p class="center"><b>Technologies used: </b>' + projects[i].technologies + '</p>';
+		if (projects[i].url != '') {
+			string += '<a class="projectLink right" href="' + projects[i].url + '" target="_blank" >see for yourself</a>';
+		}
+	document.getElementById('popUp-content').innerHTML = string;
+}
+
+function closeSummary(){
+	document.getElementById('popUp').style.display="none";
+}
+
 function printSkills(){
 	for (var i = 0; i < skills.length; i++) {
 		var string = '';
-		string += '<div class="project clearfix">';
+		string += '<div class="project clearfix divHover">';
 		string += '<h3>' + skills[i].name + '</h3>';
 		string += '<p>' + skills[i].description + '</p>'
 		string += '</div>';
